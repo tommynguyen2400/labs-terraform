cc95466b90937338c6ea3341435c4c618d2760a0
tommynguyen2400 <hoangviet3236@gmail.com>
2025-01-10 22:51:57 +0700

    update code

--- section4/input/variables.tf
+++ section4/input/variables.tf
@@ -19,9 +19,44 @@ variable "separator" {
 }
 
 variable "content1" {
-  type = map
+  type = map(any)
   default = {
     "statement1" = "1"
     "statement2" = "2"
   }
+}
+
+# type map
+variable "subject" {
+  type = map
+  default = {
+    sub1 = "math"
+    sub2 = "technology"
+  }
+}
+
+
+# type object
+variable "tom" {
+  type = object({
+    name         = string
+    color        = string
+    age          = string
+    food         = list(string)
+    favorite_pet = bool
+  })
+
+  default = {
+    name         = "Tom"
+    color        = "blue"
+    age          = "24"
+    food         = ["mouse", "fruit"]
+    favorite_pet = true
+  }
+}
+
+# type tuple
+variable "jerry" {
+  type    = tuple([string, number, bool])
+  default = ["value", 0, false]
 }
